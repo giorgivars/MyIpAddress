c8a58dec0174386a617bf0509f4fe0be3d4be24a
Jérémie Robert <appydo+jerob@gmail.com>
2018-08-25 19:17:20 +0200

    Add API

--- web.js
+++ web.js
@@ -9,6 +9,15 @@ app.get('/', function(request, response){
 	response.send(getClientAddress(request));
 });
 
+app.get('/api/ipaddress.json', function(request, response){
+	var getClientAddress = function (req) {
+	    return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+	        || req.connection.remoteAddress;
+	};
+	// response.send(getClientAddress(request));
+	response.json(getClientAddress(request));
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("Listening on " + port))
