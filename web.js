ed820ae7a1ade3f5af9e6f671d12bc36d67f0662
Jérémie Robert <appydo+jerob@gmail.com>
2022-08-25 22:35:54 +0200

    Get public ip Using built-in http library and public API

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var http = require('http');
 
 app.get('/', function(request, response){
 	var getClientAddress = function (req) {
@@ -16,6 +17,13 @@ app.get('/api/ipaddress.json', function(request, response){
 	};
 	// response.send(getClientAddress(request));
 	response.json({userAgent:request.headers['user-agent'],method:request.method,fresh:request.fresh,xhr:request.xhr,protocol:request.protocol,ipAddress:getClientAddress(request),remoteAddress:request.connection.remoteAddress,forwarded:request.headers['x-forwarded-for']});
+});
+
+# Using built-in http library and public API from https://www.ipify.org/
+http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+  resp.on('data', function(ip) {
+    console.log("My public IP address is: " + ip);
+  });
 });
 
 var port = process.env.PORT || 3000;
